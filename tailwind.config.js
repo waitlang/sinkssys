93433c0fd9e457862a300a7b5e05b4acde27bb55
ShivTheGooseman <brensdeguzman7@gmail.com>
2024-10-11 22:09:06 +0800

    Installed tailwind and flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*./*.[html]"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+
+  plugins: [
+      require('flowbite/plugin')
+  ]
+
+}
+
